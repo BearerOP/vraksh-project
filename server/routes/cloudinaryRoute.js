6e6ea70b5e576bfa7a87ae1e660f0deb14f4051d
BearerOP <ankity1892003@gmail.com>
2025-04-15 00:17:48 +0530

    feat: Move cloudinaryHandler implementation to cloudinaryRoute for better organization

--- server/routes/cloudinaryRoute.js
+++ server/routes/cloudinaryRoute.js
@@ -1,9 +1,39 @@
 const express = require("express");
 const router = express.Router();
-
-const {cloudinaryHandler} = require("../controllers/cloudinaryController");
+const cloudinary = require("cloudinary").v2;
 
 const { protect } = require("../middleware/auth");
+
+
+cloudinary.config({
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
+});
+
+const cloudinaryHandler = async (req, res) => {
+  if (req.method !== "POST") return res.status(405).end();
+
+  const { public_id, upload_preset } = req.body;
+
+  const timestamp = Math.floor(Date.now() / 1000);
+
+  const signature = cloudinary.utils.api_sign_request(
+    {
+      timestamp,
+      upload_preset,
+      public_id,
+    },
+    process.env.CLOUDINARY_API_SECRET
+  );
+
+  res.status(200).json({
+    signature,
+    timestamp,
+    apiKey: process.env.CLOUDINARY_API_KEY,
+    cloudName: process.env.CLOUDINARY_CLOUD_NAME,
+  });
+};
 
 router.post("/cloudinary-signature", protect, cloudinaryHandler);
 
